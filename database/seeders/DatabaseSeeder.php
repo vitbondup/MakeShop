28f2927bd83af87187a2af0583700aa6429937d7
vitbondup <kontent.name@ukr.net>
2022-10-19 13:27:18 +0300

    додай три сідери

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,5 +14,13 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        $this->call(CategoryTableSeeder::class);
+        $this->command->info('Дані були завантажені у таблицю категорій!');
+
+        $this->call(BrandTableSeeder::class);
+        $this->command->info('Дані були завантажені у таблицю брендів!');
+
+        $this->call(ProductTableSeeder::class);
+        $this->command->info('Дані були завантажені у таблицю товарів!');
     }
 }
