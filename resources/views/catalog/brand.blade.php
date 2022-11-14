63374366136cc1844ebf7f3fab1da4b49b7a9251
vitbondup <kontent.name@ukr.net>
2022-11-14 14:06:58 +0200

    Update brand.blade.php

--- resources/views/catalog/brand.blade.php
+++ resources/views/catalog/brand.blade.php
@@ -1,10 +1,14 @@
-<h1>Бренд: {{ $brand->name }}</h1>
-<ul>
-    @foreach ($products as $product)
-        <li>
-            <a href="{{ route('catalog.product', ['slug' => $product->slug]) }}">
-                {{ $product->name }}
-            </a>
-        </li>
-    @endforeach
-</ul>
+@extends('layout.site')
+
+@section('content')
+    <h1>Бренд: {{ $brand->name }}</h1>
+    <ul>
+        @foreach ($products as $product)
+            <li>
+                <a href="{{ route('catalog.product', ['slug' => $product->slug]) }}">
+                    {{ $product->name }}
+                </a>
+            </li>
+        @endforeach
+    </ul>
+@endsection
