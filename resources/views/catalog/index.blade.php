bb6c4b64fbd2acd44c0f2152bea19bda008453a0
vitbondup <kontent.name@ukr.net>
2022-11-14 16:38:44 +0200

    Update index.blade.php

--- resources/views/catalog/index.blade.php
+++ resources/views/catalog/index.blade.php
@@ -12,20 +12,7 @@
     <h2>Розділи каталога</h2>
     <div class="row">
         @foreach ($roots as $root)
-            <div class="col-md-6 mb-4">
-                <div class="card">
-                    <div class="card-header">
-                        <h4>{{ $root->name }}</h4>
-                    </div>
-                    <div class="card-body p-0">
-                        <img src="https://via.placeholder.com/400x120" alt="" class="img-fluid">
-                    </div>
-                    <div class="card-footer">
-                        <a href="{{ route('catalog.category', ['slug' => $root->slug]) }}"
-                           class="btn btn-dark">Перейти в разділ</a>
-                    </div>
-                </div>
-            </div>
+            @include('catalog.part.category', ['category' => $root])
         @endforeach
     </div>
 @endsection
