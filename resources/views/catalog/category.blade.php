1d7ca78b53c5b1039a7d5c47f6859c162eadd855
vitbondup <kontent.name@ukr.net>
2022-11-14 14:05:56 +0200

    Update category.blade.php

--- resources/views/catalog/category.blade.php
+++ resources/views/catalog/category.blade.php
@@ -1,10 +1,14 @@
-<h1>Категорія: {{ $category->name }}</h1>
-<ul>
-    @foreach ($products as $product)
-        <li>
-            <a href="{{ route('catalog.product', ['slug' => $product->slug]) }}">
-                {{ $product->name }}
-            </a>
-        </li>
-    @endforeach
-</ul>
+@extends('layout.site')
+
+@section('content')
+    <h1>Категорія: {{ $category->name }}</h1>
+    <ul>
+        @foreach ($products as $product)
+            <li>
+                <a href="{{ route('catalog.product', ['slug' => $product->slug]) }}">
+                    {{ $product->name }}
+                </a>
+            </li>
+        @endforeach
+    </ul>
+@endsection
