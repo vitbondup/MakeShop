93614409c94de24f9619a68a4694847ae7cb493c
vitbondup <kontent.name@ukr.net>
2022-11-14 16:25:35 +0200

    правки шаблонів

--- resources/views/catalog/brand.blade.php
+++ resources/views/catalog/brand.blade.php
@@ -2,13 +2,25 @@
 
 @section('content')
     <h1>Бренд: {{ $brand->name }}</h1>
-    <ul>
+
+    <p>{{ $brand->content }}</p>
+
+    <div class="row">
         @foreach ($products as $product)
-            <li>
-                <a href="{{ route('catalog.product', ['slug' => $product->slug]) }}">
-                    {{ $product->name }}
-                </a>
-            </li>
+            <div class="col-md-6 mb-4">
+                <div class="card">
+                    <div class="card-header">
+                        <h4>{{ $product->name }}</h4>
+                    </div>
+                    <div class="card-body p-0">
+                        <img src="https://via.placeholder.com/400x120" alt="" class="img-fluid">
+                    </div>
+                    <div class="card-footer">
+                        <a href="{{ route('catalog.product', ['slug' => $product->slug]) }}"
+                           class="btn btn-dark">Перейти до товару</a>
+                    </div>
+                </div>
+            </div>
         @endforeach
-    </ul>
+    </div>
 @endsection
--- resources/views/catalog/product.blade.php
+++ resources/views/catalog/product.blade.php
@@ -1,19 +1,45 @@
 @extends('layout.site')
 
 @section('content')
-    <h1>Товар: {{ $product->name }}</h1>
-    <p>Ціна: {{ number_format($product->price, 2, '.', '') }}</p>
-    <p>
-        Категорія:
-        <a href="{{ route('catalog.category', ['slug' => $product->category_slug]) }}">
-            {{ $product->category_name }}
-        </a>
-    </p>
-    <p>
-        Бренд:
-        <a href="{{ route('catalog.brand', ['slug' => $product->brand_slug]) }}">
-            {{ $product->brand_name }}
-        </a>
-    </p>
-    <p>{{ $product->content }}</p>
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-header">
+                    <h1>Товар: {{ $product->name }}</h1>
+                </div>
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <img src="https://via.placeholder.com/400x400"
+                                 alt="" class="img-fluid">
+                        </div>
+                        <div class="col-md-6">
+                            <p>Ціна: {{ number_format($product->price, 2, '.', '') }}</p>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-12">
+                            <p class="mt-4 mb-0">{{ $product->content }}</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="card-footer">
+                    <div class="row">
+                        <div class="col-md-6">
+                            Категорія:
+                            <a href="{{ route('catalog.category', ['slug' => $product->category_slug]) }}">
+                                {{ $product->category_name }}
+                            </a>
+                        </div>
+                        <div class="col-md-6 text-right">
+                            Бренд:
+                            <a href="{{ route('catalog.brand', ['slug' => $product->brand_slug]) }}">
+                                {{ $product->brand_name }}
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
