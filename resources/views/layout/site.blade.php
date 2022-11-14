241b33015aea81103b763e6f4c6a0f378e6d1101
vitbondup <kontent.name@ukr.net>
2022-11-14 13:39:17 +0200

    Update site.blade.php

--- resources/views/layout/site.blade.php
+++ resources/views/layout/site.blade.php
@@ -10,6 +10,50 @@
     <script src="{{ asset('js/app.js') }}"></script>
 </head>
 <body>
+<div class="container">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <!-- Бренд і кнопка «Гамбургер» -->
+        <a class="navbar-brand" href="/">Магазин</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse"
+                data-target="#navbar-example" aria-controls="navbar-example"
+                aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <!-- Основна частина меню (може мати посилання, форми та інше) -->
+        <div class="collapse navbar-collapse" id="navbar-example">
+            <!-- Цей блок розташований зліва -->
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Каталог</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Доставка</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Контакти</a>
+                </li>
+            </ul>
+            <!-- Цей блок розташований справа -->
+            <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="search"
+                       placeholder="Пошук по каталогу" aria-label="Search">
+                <button class="btn btn-outline-info my-2 my-sm-0"
+                        type="submit">Шукати</button>
+            </form>
+        </div>
+    </nav>
 
+    <div class="row">
+        <div class="col-md-3">
+            <h4>Розділи каталога</h4>
+            <p>Тут будуть корневі разділи</p>
+            <h4>Популярні бренди</h4>
+            <p>Тут будуть популярні бренди</p>
+        </div>
+        <div class="col-md-9">
+            @yield('content')
+        </div>
+    </div>
+</div>
 </body>
 </html>
