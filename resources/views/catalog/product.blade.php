99b38ff7466a9a6bbdb82864fc2a2f86e306d6be
vitbondup <kontent.name@ukr.net>
2022-11-14 13:01:02 +0200

    додай шаблон /catalog/product.blade.php

---
+++ resources/views/catalog/product.blade.php
@@ -0,0 +1,15 @@
+<h1>Товар: {{ $product->name }}</h1>
+<p>Ціна: {{ number_format($product->price, 2, '.', '') }}</p>
+<p>
+    Категорія:
+    <a href="{{ route('catalog.category', ['slug' => $product->category_slug]) }}">
+        {{ $product->category_name }}
+    </a>
+</p>
+<p>
+    Бренд:
+    <a href="{{ route('catalog.brand', ['slug' => $product->brand_slug]) }}">
+        {{ $product->brand_name }}
+    </a>
+</p>
+<p>{{ $product->content }}</p>
