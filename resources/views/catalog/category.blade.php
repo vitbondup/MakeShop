4d965ad6fa1a83023cc580d91bfe6c8228054389
vitbondup <kontent.name@ukr.net>
2022-11-14 16:31:42 +0200

    правки шаблонів

--- resources/views/catalog/category.blade.php
+++ resources/views/catalog/category.blade.php
@@ -1,26 +1,13 @@
 @extends('layout.site')
 
 @section('content')
-    <h1>Категорія: {{ $category->name }}</h1>
+    <h1>{{ $category->name }}</h1>
 
     <p>{{ $category->content }}</p>
 
     <div class="row">
         @foreach ($products as $product)
-            <div class="col-md-6 mb-4">
-                <div class="card">
-                    <div class="card-header">
-                        <h4>{{ $product->name }}</h4>
-                    </div>
-                    <div class="card-body p-0">
-                        <img src="https://via.placeholder.com/400x120" alt="" class="img-fluid">
-                    </div>
-                    <div class="card-footer">
-                        <a href="{{ route('catalog.product', ['slug' => $product->slug]) }}"
-                           class="btn btn-dark">Перейти до товару</a>
-                    </div>
-                </div>
-            </div>
+            @include('catalog.part.product', ['product' => $product])
         @endforeach
     </div>
 @endsection
