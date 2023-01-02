37021b0394a120f87e42c984e4519d8fe8d11612
vitbondup <kontent.name@ukr.net>
2023-01-02 14:05:49 +0200

    Create BasketController.php

---
+++ app/Http/Controllers/BasketController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class BasketController extends Controller {
+    public function index() {
+        return view('basket.index');
+    }
+
+    public function checkout() {
+        return view('basket.checkout');
+    }
+}
