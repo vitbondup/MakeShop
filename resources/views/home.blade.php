82a4eec7aa6cbe3d82819aa8f8ba81abcac71b0c
vitbondup <kontent.name@ukr.net>
2023-01-09 12:49:27 +0200

    правка шаблонів аутентифікації

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,11 @@
-@extends('layouts.app')
+@extends('layout.site')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+    <h1>Особистий кабінет</h1>
+    <p>Ласкаво просимо, {{ auth()->user()->name }}</p>
+    <p>Це особистий кабінет постійного покупця нашого інтернет магазину.</p>
+    <form action="{{ route('logout') }}" method="post">
+        @csrf
+        <button type="submit" class="btn btn-primary">Вийти</button>
+    </form>
 @endsection
